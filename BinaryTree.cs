39eceaf5bfabefb6f2242de0656c2c8a0b22224e
Tellexxii <tellexxii@gmail.com>
2019-04-12 13:24:08 +0500

    Create BinaryTree.cs

---
+++ BinaryTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class BSTNode<T>
+    {
+        public int NodeKey; // ключ узла
+        public T NodeValue; // значение в узле
+        public BSTNode<T> Parent; // родитель или null для корня
+        public BSTNode<T> LeftChild; // левый потомок
+        public BSTNode<T> RightChild; // правый потомок	
+
+        public BSTNode(int key, T val, BSTNode<T> parent)
+        {
+            NodeKey = key;
+            NodeValue = val;
+            Parent = parent;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+
+    // промежуточный результат поиска
+    public class BSTFind<T>
+    {
+        // null если не найден узел, и в дереве только один корень
+        public BSTNode<T> Node;
+
+        // true если узел найден
+        public bool NodeHasKey;
+
+        // true, если родительскому узлу надо добавить новый левым
+        public bool ToLeft;
+
+        public BSTFind() { Node = null; }
+    }
+
+    public class BST<T>
+    {
+        BSTNode<T> Root; // корень дерева, или null
+
+        private int count;
+
+        public BST(BSTNode<T> node)
+        {
+            Root = node;
+            count = 0;
+        }
+
+        public BSTFind<T> FindNodeByKey(int key)
+        {
+            // ищем в дереве узел и сопутствующую информацию по ключу
+            return null;
+        }
+
+        public bool AddKeyValue(int key, T val)
+        {
+            // добавляем ключ-значение в дерево
+            return false; // если ключ уже есть
+        }
+
+        public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
+        {
+            // ищем максимальное/минимальное в поддереве
+            return null;
+        }
+
+        public bool DeleteNodeByKey(int key)
+        {
+            // удаляем узел по ключу
+            return false; // если узел не найден
+        }
+
+        public int Count()
+        {
+            return count; // количество узлов в дереве
+        }
+
+    }
+}
