e322834e5652b9a1ce6778ab3f028a91fba532fb
Tellexxii <tellexxii@gmail.com>
2019-04-23 17:41:26 +0500

    Update BalancedBST

--- BalancedBST.cs
+++ BalancedBST.cs
@@ -8,25 +8,51 @@ namespace AlgorithmsDataStructures2
         public static int[] GenerateBBSTArray(int[] a)
         {
             Array.Sort(a);
+
+            int depth = CheckDepth(a.Length);
+            
             var list = new List<int>();
             int middle = a.Length / 2;
             list.Add(a[middle]);
-
+            //list.AddRange(Checking(a, 0, a.Length-1));
+            
             var llist = new List<int>();
             var rlist = new List<int>();
 
             llist.AddRange(Checking(a, 0, middle - 1));
-            rlist.AddRange(Checking(a, middle + 1, a.Length-1));
+            rlist.AddRange(Checking(a, middle + 1, a.Length - 1));
 
             int index = 0;
             while (index != llist.Count && index != rlist.Count)
             {
                 list.Add(llist[index]);
+                //list.Add(rlist[index]);
+                index++;
+            }
+            index = 0;
+            while (index != llist.Count && index != rlist.Count)
+            {
+                //list.Add(llist[index]);
                 list.Add(rlist[index]);
                 index++;
             }
+
+
             return list.ToArray();
         }
+        //public static List<int> Checking(int[] nums, int start, int end)
+        //{
+        //    if (start > end) return new List<int>();
+
+        //    int middle = start + (end - start) / 2;
+        //    var list = new List<int>();
+
+        //    list.Add(nums[middle]);
+        //    list.AddRange(Checking(nums, start, middle - 1));
+        //    list.AddRange(Checking(nums, middle + 1, end));
+
+        //    return list;
+        //}
         public static List<int> Checking(int[] nums, int start, int end)
         {
             if (start > end) return new List<int>();
@@ -39,6 +65,20 @@ namespace AlgorithmsDataStructures2
             list.AddRange(Checking(nums, middle + 1, end));
 
             return list;
+        }
+
+        public static int CheckDepth(int size)
+        {
+            int depth = 0;
+            while (true)
+            {
+
+                if ((int)Math.Pow(2, (Convert.ToDouble(depth + 1))) - 1 >= size && size <= (int)Math.Pow(2, (Convert.ToDouble(depth + 2))) - 1)
+                {
+                    return depth;
+                }
+                depth++;
+            }
         }
     }
 }
--- Tests.cs
+++ Tests.cs
@@ -1,158 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace AlgorithmsDataStructures2
 {
-    public class Tests
+    public static class Tests
     {
-        public static void TreeFindNodesByValue()
+        public static void FindKeyIndex()
         {
-            SimpleTreeNode<int> node9 = new SimpleTreeNode<int>(9,null);
-            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, node9);
-            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, node9);
-            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
-            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
-            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
-            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
-            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
-            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
-            
+            aBST tree = new aBST(3);
+            tree.Tree[0] = 50;
+            tree.Tree[1] = 25;
+            tree.Tree[2] = 75;
+            tree.Tree[4] = 37;
+            tree.Tree[5] = 62;
+            tree.Tree[6] = 84;
+            tree.Tree[9] = 31;
+            tree.Tree[10] = 43;
+            tree.Tree[11] = 55;
+            tree.Tree[14] = 92;
 
-            SimpleTree<int> tree = new SimpleTree<int>(node9);
-
-            tree.AddChild(node9,node4);
-            tree.AddChild(node9, node17);
-            tree.AddChild(node4, node3);
-            tree.AddChild(node4, node6);
-            tree.AddChild(node6, node5);
-            tree.AddChild(node6, node7);
-            tree.AddChild(node17, node22);
-            tree.AddChild(node22, node20);
-
-            List<SimpleTreeNode<int>> list = tree.FindNodesByValue(6);
-            
-            Console.WriteLine(list.Count == 1 ? "PASSED" : "ERROR, TREE FIND NODE BY VALUE");
+            Console.WriteLine(tree.FindKeyIndex(93));
         }
-        public static void TreeDeleteNode()
+        public static void AddKey()
         {
-            SimpleTreeNode<int> node9 = new SimpleTreeNode<int>(9, null);
-            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, node9);
-            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, node9);
-            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
-            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
-            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
-            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
-            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
-            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
-
-
-            SimpleTree<int> tree = new SimpleTree<int>(node9);
-
-            tree.AddChild(node9, node4);
-            tree.AddChild(node9, node17);
-            tree.AddChild(node4, node3);
-            tree.AddChild(node4, node6);
-            tree.AddChild(node6, node5);
-            tree.AddChild(node6, node7);
-            tree.AddChild(node17, node22);
-            tree.AddChild(node22, node20);
-
-            tree.DeleteNode(node6);
-            List<SimpleTreeNode<int>> list = tree.FindNodesByValue(6);
-
-            Console.WriteLine(list == null ? "PASSED" : "ERROR, DELETE NODE");
+            aBST tree = new aBST(3);
+            //tree.AddKey(50);
+            tree.AddKey(50);
+            tree.AddKey(25);
+            tree.AddKey(75);
+            tree.AddKey(37);
+            tree.AddKey(62);
+            tree.AddKey(84);                   
+            tree.AddKey(31);                   
+            tree.AddKey(43);                   
+            tree.AddKey(55);                   
+            tree.AddKey(92);
+            Console.WriteLine(tree.AddKey(24));
         }
-        public static void TreeLeafCount()
+        public static void BalancedTree()
         {
-            SimpleTreeNode<int> node9 = new SimpleTreeNode<int>(9, null);
-            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, node9);
-            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, node9);
-            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
-            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
-            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
-            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
-            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
-            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
+            int[] unsortedArray = { 1,2,3,4,12,13,14,15,16,17,18};
+            //int[] unsortedArray = { 1, 2, 3 };
 
-
-            SimpleTree<int> tree = new SimpleTree<int>(node9);
-
-            tree.AddChild(node9, node4);
-            tree.AddChild(node9, node17);
-            tree.AddChild(node4, node3);
-            tree.AddChild(node4, node6);
-            tree.AddChild(node6, node5);
-            tree.AddChild(node6, node7);
-            tree.AddChild(node17, node22);
-            tree.AddChild(node22, node20);
-
-            //SimpleTree<int> tree2 = new SimpleTree<int>(new SimpleTreeNode<int>(0,null));
-            //Console.WriteLine(tree2.LeafCount());
-            Console.WriteLine(tree.LeafCount() == 4 ? "PASSED" : "ERROR, LEAF COUNT");
-            //Console.WriteLine(tree.LeafCount());
-
+            var sortedArray = BalancedBST.GenerateBBSTArray(unsortedArray);
+            foreach (var item in sortedArray)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine(BalancedBST.CheckDepth(16));
         }
-        public static void TreeNodesCount()
+        public static void CreateTree()
         {
-            SimpleTreeNode<int> node9 = new SimpleTreeNode<int>(9, null);
-            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, node9);
-            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, node9);
-            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
-            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
-            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
-            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
-            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
-            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
+            BST<int> tree = new BST<int>(new BSTNode<int>(8,8,null));
+            var arr = BalancedBST.GenerateBBSTArray(new int[] { 1, 2, 3, 4, 12, 13, 14, 15, 16, 17 });
+            Console.WriteLine(arr.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tree.AddKeyValue(arr[i],arr[i]);
+            }
 
-
-            SimpleTree<int> tree = new SimpleTree<int>(node9);
-
-            tree.AddChild(node9, node4);
-            tree.AddChild(node9, node17);
-            tree.AddChild(node4, node3);
-            tree.AddChild(node4, node6);
-            tree.AddChild(node6, node5);
-            tree.AddChild(node6, node7);
-            tree.AddChild(node17, node22);
-            tree.AddChild(node22, node20);
-
-            List<SimpleTreeNode<int>> list = tree.GetAllNodes();
-            Console.WriteLine(list.Count == 9 ? "PASSED" : "ERROR, NODES TO LIST");
-            
+            var list = tree.DeepAllNodes(0);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.NodeKey);
+            }
         }
-        public static void TreeMoveNodes()
-        {
-            SimpleTreeNode<int> node9 = new SimpleTreeNode<int>(9, null);
-            SimpleTreeNode<int> node4 = new SimpleTreeNode<int>(4, node9);
-            SimpleTreeNode<int> node17 = new SimpleTreeNode<int>(17, node9);
-            SimpleTreeNode<int> node3 = new SimpleTreeNode<int>(3, node4);
-            SimpleTreeNode<int> node6 = new SimpleTreeNode<int>(6, node4);
-            SimpleTreeNode<int> node5 = new SimpleTreeNode<int>(5, node6);
-            SimpleTreeNode<int> node7 = new SimpleTreeNode<int>(7, node6);
-            SimpleTreeNode<int> node22 = new SimpleTreeNode<int>(22, node17);
-            SimpleTreeNode<int> node20 = new SimpleTreeNode<int>(20, node22);
-
-
-            SimpleTree<int> tree = new SimpleTree<int>(node9);
-
-            tree.AddChild(node9, node4);
-            tree.AddChild(node9, node17);
-            tree.AddChild(node4, node3);
-            tree.AddChild(node4, node6);
-            tree.AddChild(node6, node5);
-            tree.AddChild(node6, node7);
-            tree.AddChild(node17, node22);
-            tree.AddChild(node22, node20);
-
-            //Console.WriteLine(node6.Parent.NodeValue);
-            
-            tree.MoveNode(node6,node22);
-            
-            //Console.WriteLine(node6.Parent.NodeValue);
-            
-
-            Console.WriteLine(node6.Parent.NodeValue == 22 ? "PASSED" : "ERROR, NODES TO LIST");
-        }
-
     }
 }
