d9cdaf890f7e237e9c6f604bee5d95c5ff502836
Tellexxii <tellexxii@gmail.com>
2020-02-27 19:00:40 +0500

    added DepthFirstSearch

--- SimpleGraph.cs
+++ SimpleGraph.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsDataStructures2
 {
-    public class Vertex
+    public class Vertex<T>
     {
-        public int Value;
-        public Vertex(int val)
+        public bool Hit;
+        public T Value;
+        public Vertex(T val)
         {
             Value = val;
+            Hit = false;
         }
     }
 
-    public class SimpleGraph
+    public class SimpleGraph<T>
     {
-        public Vertex[] vertex;
+        public Vertex<T>[] vertex;
         public int[,] m_adjacency;
         public int max_vertex;
 
@@ -22,10 +25,10 @@ namespace AlgorithmsDataStructures2
         {
             max_vertex = size;
             m_adjacency = new int[size, size];
-            vertex = new Vertex[size];
+            vertex = new Vertex<T>[size];
         }
 
-        public void AddVertex(int value)
+        public void AddVertex(T value)
         {
             // ваш код добавления новой вершины 
             // с значением value 
@@ -35,7 +38,7 @@ namespace AlgorithmsDataStructures2
             {
                 if (vertex[i] == null)
                 {
-                    vertex[i] = new Vertex(value); ;
+                    vertex[i] = new Vertex<T>(value); ;
                     break;
                 }
             }
@@ -73,6 +76,50 @@ namespace AlgorithmsDataStructures2
             // удаление ребра между вершинами v1 и v2
             m_adjacency[v1, v2] = 0;
             m_adjacency[v2, v1] = 0;
+        }
+
+        public List<Vertex<T>> DepthFirstSearch(int VFrom, int VTo)
+        {
+            List<Vertex<T>> path = new List<Vertex<T>>();
+            Stack<Vertex<T>> vertices = new Stack<Vertex<T>>();
+            
+            foreach (var item in vertex) { item.Hit = false; }
+            
+            Vertex<T> currentVertex = vertex[VFrom];
+            currentVertex.Hit = true;
+            vertices.Push(currentVertex);
+            
+            while (true)
+            {
+                if (IsEdge(Array.IndexOf(vertex, currentVertex), VTo))
+                {
+                    vertices.Push(vertex[VTo]);
+                    path.AddRange(vertices);
+                    path.Reverse();
+                    return path;
+                }
+
+                List<Vertex<T>> adjVertex = new List<Vertex<T>>();
+                
+                adjVertex.AddRange(Array.FindAll(vertex, (item) =>
+                    !item.Hit &&
+                    item != currentVertex &&
+                    IsEdge(Array.IndexOf(vertex, currentVertex), Array.IndexOf(vertex, item))));
+
+                if (!adjVertex.Any())
+                {
+                    vertices.Pop();
+                    if (!vertices.Any()) return path;
+                    currentVertex = vertices.Peek();
+                    currentVertex.Hit = true;
+                }
+                else
+                {
+                    currentVertex = adjVertex[0];
+                    currentVertex.Hit = true;
+                    vertices.Push(currentVertex);
+                }
+            }
         }
     }
 }
