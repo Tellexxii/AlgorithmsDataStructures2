8713de3e55f0091f813ab7ed57317df4bc19008b
Tellexxii <tellexxii@gmail.com>
2020-11-27 13:35:52 +0500

    -

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -19,7 +19,6 @@ namespace AlgorithmsDataStructures2
             LeftChild = null;
             RightChild = null;
         }
-        
     }
 
     // промежуточный результат поиска
@@ -35,85 +34,70 @@ namespace AlgorithmsDataStructures2
         public bool ToLeft;
 
         public BSTFind() { Node = null; }
-
-        public BSTFind(BSTNode<T> node, bool nodeHasKey, bool toLeft)
-        {
-            Node = node;
-            NodeHasKey = nodeHasKey;
-            ToLeft = toLeft;
-        }
     }
 
     public class BST<T>
     {
-        public BSTNode<T> Root; // корень дерева, или null
-
-        private int count;
+        BSTNode<T> Root; // корень дерева, или null
 
         public BST(BSTNode<T> node)
         {
             Root = node;
-            count = Root == null ? 0 : 1;
         }
 
         public BSTFind<T> FindNodeByKey(int key)
         {
-            // ищем в дереве узел и сопутствующую информацию по ключу
-            BSTNode<T> node = Root;
+            if (Root == null) return null;
 
-            if (node == null) return null;
+            BSTNode<T> currentNode = Root;
+            BSTNode<T> parrent;
 
-            while (node != null)
+            while (!key.Equals(currentNode.NodeKey))
             {
-                int result = key.CompareTo(node.NodeKey);
-                if (result < 0) // key less, go left
+                parrent = currentNode;
+
+                if (key >= currentNode.NodeKey)
                 {
-                    if(node.LeftChild != null)
-                    {
-                        node = node.LeftChild;
-                        continue;
-                    }
-                    return new BSTFind<T>(node, false, true); // key not found, LeftChild is null
-                }
-                else if(result > 0) // key greater, go right
-                {
-                    if (node.RightChild != null)
-                    {
-                        node = node.RightChild;
-                        continue;
-                    }
-                    return new BSTFind<T>(node, false, false); // key not found
-                }
-                return new BSTFind<T>(node, true, false);
-            }
-            return null;
-        }
-        
-        public bool AddKeyValue(int key, T val)
-        {
-            // добавляем ключ-значение в дерево
-            BSTFind<T> find = this.FindNodeByKey(key);
-            var node = find.Node;
-            if (Root == null)
-            {
-                Root = node;
-                count++;
-                return true;
-            }
-            if (node.NodeKey == key) return false;
-            else
-            {
-                if (node.NodeKey > key)
-                {
-                    node.LeftChild = new BSTNode<T>(key, val, node);
+                    currentNode = currentNode.RightChild; // двигаемся вправо
+                    if (currentNode == null)
+                        return new BSTFind<T> {Node = parrent, NodeHasKey = false, ToLeft = false};
                 }
                 else
                 {
-                    node.RightChild = new BSTNode<T>(key, val, node);
+                    currentNode = currentNode.LeftChild;
+                    if (currentNode == null)
+                    {
+                        return new BSTFind<T> {Node = parrent, NodeHasKey = false, ToLeft = true};
+                    }
                 }
             }
-            count++;
-            return true;
+
+            return new BSTFind<T> { Node = currentNode, NodeHasKey = true }; // найденный узел
+        }
+
+        public bool AddKeyValue(int key, T val)
+        {
+            // добавляем ключ-значение в дерево, если корень не null
+            if (Root != null)
+            {
+                BSTFind<T> foundNode = FindNodeByKey(key);
+
+                if (!foundNode.NodeHasKey)
+                {
+                    if (foundNode.ToLeft)
+                        foundNode.Node.LeftChild = new BSTNode<T>(key, val, foundNode.Node);
+                    else
+                        foundNode.Node.RightChild = new BSTNode<T>(key, val, foundNode.Node);
+
+                    return true;
+                }
+            }
+            else
+            {
+                Root = new BSTNode<T>(key, val, null);
+            }
+
+            return false;
         }
 
         public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
@@ -121,213 +105,101 @@ namespace AlgorithmsDataStructures2
             // ищем максимальное/минимальное в поддереве
             BSTNode<T> current = FromNode;
 
-            if (FromNode == null) return null;
-
             if (FindMax)
             {
-                while(current.RightChild != null) current = current.RightChild;
-                return current;
+                if (current.RightChild != null)
+                    current = FinMinMax(current.RightChild, FindMax);
             }
             else
             {
-                while (current.LeftChild != null) current = current.LeftChild;
-                return current;
+                if (current.LeftChild != null)
+                    current = FinMinMax(current.LeftChild, FindMax);
             }
+
+            return current;
         }
 
         public bool DeleteNodeByKey(int key)
         {
-            BSTFind<T> found = FindNodeByKey(key);
-            if (found.NodeHasKey)
+            BSTFind<T> foundNode = FindNodeByKey(key);
+            if (!foundNode.NodeHasKey) return false;
+            if (foundNode.Node.LeftChild == null && foundNode.Node.RightChild == null)
             {
-                // No child
-                if (found.Node.LeftChild == null && found.Node.RightChild == null)
+                if (foundNode.Node.Parent.LeftChild != null && foundNode.Node.Parent.LeftChild.Equals(foundNode.Node))
+                    foundNode.Node.Parent.LeftChild = null;
+                else if (foundNode.Node.Parent.RightChild != null && foundNode.Node.Parent.RightChild.Equals(foundNode.Node))
+                    foundNode.Node.Parent.RightChild = null;
+            }
+            else if (foundNode.Node.LeftChild == null ^ foundNode.Node.RightChild == null)
+            {
+                if (foundNode.Node.LeftChild != null)
                 {
-                    if (found.Node.Parent.LeftChild != null && found.Node.Parent.LeftChild.Equals(found.Node))
-                        found.Node.Parent.LeftChild = null;
-                    else if (found.Node.Parent.RightChild != null && found.Node.Parent.RightChild.Equals(found.Node))
-                        found.Node.Parent.RightChild = null;
-                }
-                // One child
-                else if (found.Node.LeftChild == null ^ found.Node.RightChild == null)
-                {
-                    if (found.Node.LeftChild != null)
-                    {
-                        if (found.Node.Parent.LeftChild != null && found.Node.Parent.LeftChild.Equals(found.Node))
-                            found.Node.Parent.LeftChild = found.Node.LeftChild;
-                        else
-                            found.Node.Parent.RightChild = found.Node.LeftChild;
+                    if (foundNode.Node.Parent.LeftChild != null && foundNode.Node.Parent.LeftChild.Equals(foundNode.Node))
+                        foundNode.Node.Parent.LeftChild = foundNode.Node.LeftChild;
+                    else
+                        foundNode.Node.Parent.RightChild = foundNode.Node.LeftChild;
 
-                        found.Node.LeftChild.Parent = found.Node.Parent;
-                    }
-                    else // Bind chuld to parent
-                    {
-                        if (found.Node.Parent.LeftChild != null && found.Node.Parent.LeftChild.Equals(found.Node))
-                            found.Node.Parent.LeftChild = found.Node.RightChild;
-                        else
-                            found.Node.Parent.RightChild = found.Node.RightChild;
-
-                        found.Node.RightChild.Parent = found.Node.Parent;
-                    }
+                    foundNode.Node.LeftChild.Parent = foundNode.Node.Parent;
                 }
-                // 2 child
                 else
                 {
-                    BSTNode<T> successorNode = FinMinMax(found.Node.RightChild, false); 
-
-                    if (successorNode.RightChild != null) 
-                    {
-                        successorNode.Parent.LeftChild = successorNode.RightChild; 
-                        successorNode.RightChild.Parent = successorNode.Parent; 
-                    }
+                    if (foundNode.Node.Parent.LeftChild != null && foundNode.Node.Parent.LeftChild.Equals(foundNode.Node))
+                        foundNode.Node.Parent.LeftChild = foundNode.Node.RightChild;
                     else
-                    {
-                        if (successorNode.Parent.LeftChild == successorNode)
-                            successorNode.Parent.LeftChild = null; 
-                        else
-                            successorNode.Parent.RightChild = null;
-                    }
-                    
-                    if (found.Node.Parent.RightChild == found.Node)
-                        found.Node.Parent.RightChild = successorNode;
+                        foundNode.Node.Parent.RightChild = foundNode.Node.RightChild;
+
+                    foundNode.Node.RightChild.Parent = foundNode.Node.Parent;
+                }
+            }
+            else
+            {
+                BSTNode<T> successorNode = FinMinMax(foundNode.Node.RightChild, false);
+
+                if (successorNode.RightChild != null)
+                {
+                    successorNode.Parent.LeftChild = successorNode.RightChild;
+                    successorNode.RightChild.Parent = successorNode.Parent;
+                }
+                else
+                {
+                    if (successorNode.Parent.LeftChild == successorNode)
+                        successorNode.Parent.LeftChild = null;
                     else
-                        found.Node.Parent.LeftChild = successorNode;
+                        successorNode.Parent.RightChild = null;
+                }
+                if (foundNode.Node.Parent.RightChild == foundNode.Node)
+                    foundNode.Node.Parent.RightChild = successorNode;
+                else
+                    foundNode.Node.Parent.LeftChild = successorNode;
 
-                    successorNode.Parent = found.Node.Parent; 
+                successorNode.Parent = foundNode.Node.Parent;
 
-                    successorNode.LeftChild = found.Node.LeftChild; 
-                    successorNode.RightChild = found.Node.RightChild; 
-
+                successorNode.LeftChild = foundNode.Node.LeftChild;
+                successorNode.RightChild = foundNode.Node.RightChild;
                     
-                    if (found.Node.RightChild != null)
-                        found.Node.RightChild.Parent = successorNode;
-                    if (found.Node.LeftChild != null)
-                        found.Node.LeftChild.Parent = successorNode;
-                }
-                count--;
-                return true;
+                if (foundNode.Node.RightChild != null)
+                    foundNode.Node.RightChild.Parent = successorNode;
+                if (foundNode.Node.LeftChild != null)
+                    foundNode.Node.LeftChild.Parent = successorNode;
             }
 
-            return false; // Not found
-        }
+            return true;
 
-        public List<BSTNode<T>> WideAllNodes()
-        {
-            var list = new List<BSTNode<T>>();
-            var queue = new Queue<BSTNode<T>>();
-            var node = Root;
-
-            if (Root == null) return list;
-
-            queue.Enqueue(node);
-            while(queue.Count != 0)
-            {
-                node = queue.Dequeue();
-                list.Add(node);
-
-                if(node.LeftChild != null)
-                {
-                    queue.Enqueue(node.LeftChild);
-                }
-                if(node.RightChild != null)
-                {
-                    queue.Enqueue(node.RightChild);
-                }
-            }
-            return list;
-        }
-
-        public List<BSTNode<T>> DeepAllNodes(int method)
-        {
-            // method - 0(in -order), 1(post - order), 2(pre - order)
-            var list = new List<BSTNode<T>>();
-            var node = Root;
-
-            if (Root == null) return list;
-            switch (method)
-            {
-                case 0:
-                    list = this.Inorder(node);
-                    break;
-                case 1:
-                    list = this.Postorder(node);
-                    break;
-                case 2:
-                    list = this.Preorder(node);
-                    break;
-                default:
-                    break;
-            }
-            return list;
-        }
-        private List<BSTNode<T>> Inorder(BSTNode<T> node)
-        {
-            var list = new List<BSTNode<T>>();
-            if (node != null)
-            {
-                if (node.LeftChild != null)
-                {
-                    list.AddRange(Inorder(node.LeftChild));
-                }
-
-                list.Add(node);
-
-                if (node.RightChild != null)
-                {
-                    list.AddRange(Inorder(node.RightChild));
-                }
-
-
-            }
-            return list;
-        }
-        private List<BSTNode<T>> Postorder(BSTNode<T> node)
-        {
-            var list = new List<BSTNode<T>>();
-            if (node != null)
-            {
-                if (node.LeftChild != null)
-                {
-                    list.AddRange(Postorder(node.LeftChild));
-                }
-                
-                if (node.RightChild != null)
-                {
-                    list.AddRange(Postorder(node.RightChild));
-                }
-
-                list.Add(node);
-
-            }
-            return list;
-        }
-        private List<BSTNode<T>> Preorder(BSTNode<T> node)
-        {
-            var list = new List<BSTNode<T>>();
-            if (node != null)
-            {
-                list.Add(node);
-
-                if (node.LeftChild != null)
-                {
-                    list.AddRange(Preorder(node.LeftChild));
-                }
-
-                if (node.RightChild != null)
-                {
-                    list.AddRange(Preorder(node.RightChild));
-                }
-
-
-            }
-            return list;
         }
 
         public int Count()
         {
-            return count; // количество узлов в дереве
+            return Recursive(Root); // количество узлов в дереве
         }
+        private int Recursive(BSTNode<T> node, int count = 0)
+        {
+            if (node == null) return count;
+            count++;
+            count = Recursive(node.LeftChild, count);
+            count = Recursive(node.RightChild, count);
 
-        
+            return count;
+        }
     }
+
 }
--- SimpleTree.cs
+++ SimpleTree.cs
@@ -33,23 +33,16 @@ namespace AlgorithmsDataStructures2
             // ваш код добавления нового дочернего узла существующему ParentNode
             if (ParentNode == null)
             {
-                NewChild.Parent = null;
                 Root = NewChild;
             }
             else
             {
-                if (ParentNode.Children != null)
-                {
-                    ParentNode.Children.Add(NewChild);
-                }
-                else
-                {
-                    ParentNode.Children = new List<SimpleTreeNode<T>>();
-                    ParentNode.Children.Add(NewChild);
-                }
+                if (ParentNode.Children == null) ParentNode.Children = new List<SimpleTreeNode<T>>();
 
-                NewChild.Parent = ParentNode;
+                ParentNode.Children.Add(NewChild);
             }
+            
+            NewChild.Parent = ParentNode;
 
             count++;
         }
@@ -57,58 +50,28 @@ namespace AlgorithmsDataStructures2
         public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
         {
             // ваш код удаления существующего узла NodeToDelete
-            SimpleTreeNode<T> current = Root;
-            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
-
-            stack.Push(current);
-
-            while (current != null && stack.Count != 0)
-            {
-                if (current.Equals(NodeToDelete))
-                {
-                    current.Parent.Children.Remove(NodeToDelete);
-                    current.Parent = null;
-                    count--;
-                    break;
-                }
-                else
-                {
-                    if (current.Children != null)
-                    {
-                        List<SimpleTreeNode<T>> children = current.Children;
-                        foreach (var child in children)
-                        {
-                            stack.Push(child);
-                        }
-                    }
-
-                }
-
-                current = stack.Pop();
-            }
         }
 
         public List<SimpleTreeNode<T>> GetAllNodes()
         {
             // ваш код выдачи всех узлов дерева в определённом порядке
-            SimpleTreeNode<T> current = Root;
+            SimpleTreeNode<T> currentNode = Root;
             Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
             List<SimpleTreeNode<T>> nodes = new List<SimpleTreeNode<T>>();
 
-            stack.Push(current);
+            stack.Push(currentNode);
 
-            while (current != null && stack.Count != 0)
+            while (currentNode != null && stack.Count != 0)
             {
-                if (current.Children != null)
+                if (currentNode.Children != null)
                 {
-                    List<SimpleTreeNode<T>> children = current.Children;
-                    foreach (var child in children)
+                    foreach (SimpleTreeNode<T> child in currentNode.Children)
                     {
                         stack.Push(child);
                     }
                 }
-                nodes.Add(current);
-                current = stack.Pop();
+                nodes.Add(currentNode);
+                currentNode = stack.Pop();
             }
 
             return nodes.Count == 0 ? null : nodes;
@@ -117,41 +80,13 @@ namespace AlgorithmsDataStructures2
         public List<SimpleTreeNode<T>> FindNodesByValue(T val)
         {
             // ваш код поиска узлов по значению
-            List<SimpleTreeNode<T>> nodes = new List<SimpleTreeNode<T>>();
-            SimpleTreeNode<T> current = Root;
-            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
-
-            stack.Push(current);
-
-            while (current != null && stack.Count != 0)
-            {
-                if(current.NodeValue.Equals(val)) nodes.Add(current);
-                else
-                {
-                    if (current.Children != null)
-                    {
-                        List<SimpleTreeNode<T>> children = current.Children;
-                        foreach (var child in children)
-                        {
-                            stack.Push(child);
-                        }
-                    }
-
-                }
-
-                current = stack.Pop();
-            }
-            return nodes.Count == 0 ? null : nodes;
+            return null;
         }
 
         public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
         {
             // ваш код перемещения узла вместе с его поддеревом -- 
             // в качестве дочернего для узла NewParent
-            OriginalNode.Parent.Children.Remove(OriginalNode);
-            this.AddChild(NewParent, OriginalNode);
-            count--;
-
         }
 
         public int Count()
@@ -162,67 +97,8 @@ namespace AlgorithmsDataStructures2
 
         public int LeafCount()
         {
-            List<SimpleTreeNode<T>> list = this.GetAllNodes();
-            int leafs = 0;
-            foreach (var node in list)
-            {
-                if (node.Children == null || node.Children.Count == 0) leafs++;
-            }
-
-            return leafs;
-        }
-        public List<int> EvenTrees()
-        {
-            if (Count() % 2 == 1) return new List<int>();
-            
-            List<SimpleTreeNode<T>> leafs = Recursive(Root).FindAll(
-                node => node.Children == null || node.Children.Count == 0);
-
-            List<int> edges = new List<int>();
-            SimpleTreeNode<T> previousLeafParrent = null;
-            foreach (var leaf in leafs)
-            {
-                if (previousLeafParrent != leaf.Parent)
-                {
-                    SimpleTreeNode<T> node = leaf.Parent;
-                    while (node != Root)
-                    {
-                        int nodesCount = Recursive(node).Count;
-                        if (nodesCount % 2 == 0)
-                        {
-                            if (typeof(T) == typeof(int))
-                            {
-                                if (!edges.Contains(Convert.ToInt32(node.Parent.NodeValue)) ||
-                                    !edges.Contains(Convert.ToInt32(node.NodeValue)))
-                                {
-                                    edges.Add(Convert.ToInt32(node.Parent.NodeValue));
-                                    edges.Add(Convert.ToInt32(node.NodeValue));
-                                }
-                            }
-                        }
-
-                        node = node.Parent;
-                    }
-                }
-
-                previousLeafParrent = leaf.Parent;
-            }
-
-            return edges;
-        }
-
-        private List<SimpleTreeNode<T>> Recursive(SimpleTreeNode<T> targetNode, T val = default(T), bool isFind = false)
-        {
-            SimpleTreeNode<T> node = targetNode;
-            var result = new List<SimpleTreeNode<T>> { node };
-
-            for (int i = 0; node.Children != null && i < node.Children.Count; i++)
-            {
-                result.AddRange(Recursive(node.Children[i]));
-            }
-
-            return result;
+            // количество листьев в дереве
+            return 0;
         }
     }
-
 }
